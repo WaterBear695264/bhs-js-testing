9967eadd6502a09d65a87705d18e97958be04b6b
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2025-08-13 18:41:51 +0000

    added 2 functions

--- script.js
+++ script.js
@@ -4,9 +4,27 @@
  */
 
 function sleep_in(weekday,vacation){
-    return true;
+    return vacation;
 }
 
 function string_times(str,num){
     return str;
+}
+
+function countEvens(arr){
+let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            count++;
+        }
+    }
+    return count
+}
+
+function countHi(string){
+let count = 0;
+    for(let i = 0; i < string.length; i++){
+        console.log(string[i]==="H")
+    }
+    return count;
 }
