49f037a759fea0d3e89b9b58e9f2c57df0db0f60
WaterBear695264 <142440070+WaterBear695264@users.noreply.github.com>
2025-10-23 18:42:27 +0000

    done most the recursion problems

--- script.js
+++ script.js
@@ -3,69 +3,49 @@
  * Run after you make changes to re-run the tests with your new code.
  */
 
-function sleep_in(weekday,vacation){
-    return vacation;
-}
-
-function string_times(str,num){
-    return str;
-}
-
-function countEvens(arr){
-let count = 0;
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] % 2 === 0){
-            count++;
-        }
-    }
-    return count
-}
 
 function countHi(string){
-let count = 0;
-    for(let i = 0; i < string.length-1; i++){
-        if(string[i] === "h" && string[i+1] === "i"){
-            count++;
-        }
+    if(string[1] == null){
+        return 0;
     }
-    return count;
-}
-
-function no14(arr){
-    return !(arr.includes(4) && arr.includes(1))
-}
-
-function either24(arr){
-let dub2 = false
-let dub4 = false
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] === 2 && arr[i+1] === 2){
-            dub2 = true
-        } else if(arr[i] === 4 && arr[i+1] === 4){
-            dub4 = true
-        }
+    if(string[0] === "h" && string[1] === "i"){
+        return 1 + countHi(string.substring(1));
+    }else {
+        return 0 + countHi(string.substring(1));
     }
-    return (dub2 || dub4) && !(dub2 && dub4);
 }
 
-function makeChocolate(small, big, goal){
-let leftover = goal%5;
-let enoughbig = big >= (goal-leftover)/5
-let enoughsmall = small >= (goal - (big*5))
-if(enoughbig && enoughsmall){
-    return leftover;
-}else if(!enoughbig && enoughsmall){
-    return goal-(big*5);
-}else{
-    return -1;
-}
-}
-
-function luckySum(a, b, c){
-    let count = 0
-    let arr = [a, b, c]
-    for(let i = 0; i<Math.max(arr.indexOf(13); i++){
-        count+=arr[i];
+function fibonacci(n){
+    if(n === 0){
+        return 0;
+    }else if(n===1){
+        return 1;
+    }else{
+        return fibonacci(n-1) + fibonacci(n-2);
     }
-    return count
+}
+
+function triangle(n){
+    if(n === 0){
+        return 0
+    }else{
+        return n + triangle(n-1)
+    }
+}
+
+function reverseString(string){
+    if(string.length === 0){
+        return "";
+    }else{
+        return string[string.length-1] + reverseString(string.slice(0, string.length - 1))
+    }
+}
+
+function palindrome(string){
+    return reverseString(string) === string
+}
+
+function sumDigits(num){
+    let a = num.toString();
+    if()
 }
